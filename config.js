d81e3be6585157f3ac04f6522920c8a94224e97a
kino1099 <110601886+kino1099@users.noreply.github.com>
2022-09-19 23:36:37 +0700

    Update config.js

--- config.js
+++ config.js
@@ -2,8 +2,8 @@ let fs = require('fs')
 let chalk = require('chalk')
 
 global.owner = [
-  ['6281511480762'],
-  ['6281511480762', 'YahyaGanzz', true]
+  ['6285693376594'],
+  ['6285693376594', 'KinoGanzz', true]
   // [number, dia creator/owner?, dia developer?]
 ] // Put your number here
 global.mods = [] // Want some help?
